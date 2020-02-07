40b6cfe3ad5d8f0cdc46f7793d0ae8b6db442a7c
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-07 16:55:42 -0600

    sum total works

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
+//-------Global Variables------//
+
 counter = 0
 allDice = [];
+diceVal = [];
 
+
+//------ OOP Class ----//
 class Die {
     constructor(value) {
         value = randomVal(1, 6)
@@ -8,38 +13,52 @@ class Die {
         this.div.classList.add('dice');
         this.div.id = counter
         this.div.innerText = value;
-        $(this.div).click(() => this.roll())
+        $(this.div).click(() => {
+            this.roll();
+            diceVal = []
+        })
         allDice.push(this)
         // (`${this.div}`).dblclick(() => {
         //     //removes div
         // })
+
         $('.diceContainer').append(this.div);
-    
+
     }
     roll() {
         let newVal = randomVal(1, 6)
         this.div.innerText = newVal
     }
+
 }
 
 
+
+//---- Button Events----//
 
 $('.getDice').on('click', function () {
     insertDice()
 })
 
 
-$('.rollDice').click(function(){
+$('.rollDice').click(function () {
     allDice.forEach((val) => {
         val.roll()
     })
+    diceVal = []
+})
+
+
+$('.sumDice').click(function () {
+    diceTotal(allDice);
+
+
+
 })
 
 
 
-$('.sumDice').click() //totals value of all dice in alert
-
-
+// --- Global Funtions ---//
 function insertDice() {
     counter++
     new Die()
@@ -49,4 +68,15 @@ function insertDice() {
 
 function randomVal(min, max) {
     return Math.floor((Math.random() * (max - min)) + min)
+}
+
+
+function diceTotal(arr) {
+    allDice.forEach((val) => {
+        let num = parseInt(val.div.innerText)
+        diceVal.push(num);
+    });
+    let answer = diceVal.reduce((acc, val) => acc + val)
+    alert(`Your total is: ${answer}`)
+
 }
